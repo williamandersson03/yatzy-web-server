c30dfbfb5507a23e0774ccf98bc560ee720dac73
William Andersson <anderssonwilliam2003@gmail.com>
2024-03-13 13:44:58 +0100

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AuthService } from './auth.service';
 import { LoginController } from './login.controller';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '192.168.1.22',
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DBNAME,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [LoginController],
   providers: [AuthService],
 })
